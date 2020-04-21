7f360e5220a1fd5bf5cb4c96e7983c1be57843de
jaenek <jaenek@protonmail.com>
2020-04-21 20:53:54 +0200

    Save theme when reopening browser

--- public/cookies.js
+++ public/cookies.js
@@ -1,23 +1,27 @@
 document.onload = getTheme();
 
 function setCookie(theme) {
-  document.cookie = "theme=" + theme + ";path=/";
+	var d = new Date();
+	d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
+	var expires = "expires="+d.toUTCString();
+	document.cookie = "theme=" + theme + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for(var i = 0; i <ca.length; i++) {
+		var c = ca[i];
+			while (c.charAt(0) == ' ') {
+		c = c.substring(1);
+		}
+
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
 }
 
 function getTheme() {
